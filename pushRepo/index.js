fa47f97e271bbd0eaf0cd3dc558e199f9c75d80d
Felipe Reinaux <freinaux@number8.com>
2022-04-25 21:40:55 -0300

    version 1

---
+++ pushRepo/index.js
@@ -0,0 +1,10 @@
+const git = require('simple-git');
+
+async function deploy() {   
+   await git().checkout('sves')
+   await git().add('.')
+   await git().commit("version 1")
+   await git().push('origin','sves');
+}
+
+deploy()

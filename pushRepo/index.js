6c5c49755617c57606dd7b4128048d0090bbb061
Felipe Reinaux <freinaux@number8.com>
2022-05-10 12:58:49 -0300

    corrigir topnavbar

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("corrigir login no ejc")
+   await git().commit("corrigir topnavbar")
    await git().push('origin','ejc');
 }
 

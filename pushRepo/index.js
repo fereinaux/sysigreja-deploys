8b43515dd7ca19617b134ef16aaeda20e208c815
Felipe Reinaux <freinaux@number8.com>
2022-05-10 12:04:59 -0300

    ajuste no campo de apelido

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('iecbeventos')
    await git().add('../.')
-   await git().commit("ajuste gitignore")
+   await git().commit("ajuste no campo de apelido")
    await git().push('origin','iecbeventos');
 }
 

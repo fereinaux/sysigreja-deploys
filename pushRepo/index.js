fcda12369cc2b2a69b41437683809e6579d4adbc
Felipe Reinaux <freinaux@number8.com>
2022-05-10 11:48:54 -0300

    ajuste gitignore

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('iecbeventos')
    await git().add('../.')
-   await git().commit("version 1")
+   await git().commit("ajuste gitignore")
    await git().push('origin','iecbeventos');
 }
 

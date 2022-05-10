7684582697cd7ccf0eaf36f24d5d68716c5e072f
Felipe Reinaux <freinaux@number8.com>
2022-05-10 12:46:26 -0300

    problema no login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -48,6 +48,7 @@ namespace SysIgreja.Controllers
         [AllowAnonymous]
         public async Task<ActionResult> Login(LoginViewModel model)
         {
+            ViewBag.Configuracao = configuracaoBusiness.GetConfiguracao();
             if (ModelState.IsValid)
             {
                 var user = await UserManager.FindAsync(model.UserName.ToLower(), model.Password.ToLower());
--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('realidaderecon')
    await git().add('../.')
-   await git().commit("version 3")
+   await git().commit("problema no login")
    await git().push('origin','realidaderecon');
 }
 

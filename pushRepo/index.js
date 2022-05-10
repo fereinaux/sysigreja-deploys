3dae0d3e7cc98b52ddd875641fe806827a25f541
Felipe Reinaux <freinaux@number8.com>
2022-05-10 12:57:41 -0300

    corrigir login no ejc

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("version 2")
+   await git().commit("corrigir login no ejc")
    await git().push('origin','ejc');
 }
 

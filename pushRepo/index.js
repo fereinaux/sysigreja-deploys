443d9ab27494d7913d4d4bbe7171c7c4b6998a5a
Felipe Reinaux <freinaux@number8.com>
2022-05-13 14:04:48 -0300

    ajuste config

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('cursilhorecon')
    await git().add('../.')
-   await git().commit("webconfig")
+   await git().commit("ajuste config")
    await git().push('origin','cursilhorecon');
 }
 

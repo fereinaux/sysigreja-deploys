08eafbe84fa2cb2fa913696aa5fe7d310ea01342
Felipe Reinaux <freinaux@number8.com>
2022-05-13 16:04:56 -0300

    ajuste genero

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('cursilhorecon')
    await git().add('../.')
-   await git().commit("ajuste cep")
+   await git().commit("ajuste genero")
    await git().push('origin','cursilhorecon');
 }
 

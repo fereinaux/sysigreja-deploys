42b48310f58a75a9c6deb875a9213d41e4c32f6e
Felipe Reinaux <freinaux@number8.com>
2022-05-15 14:02:16 -0300

    layout _inscricoes

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("ajustar inscrições")
+   await git().commit("layout _inscricoes")
    await git().push('origin','ejc');
 }
 

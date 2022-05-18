f7c074765cc4f3ff300e035a1bdea581f0b965c9
Felipe Reinaux <freinaux@number8.com>
2022-05-18 12:15:56 -0300

    navegar entre registros info

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('sves')
    await git().add('../.')
-   await git().commit("marcadores, excluir equipanteEvento, link whatsapp")
+   await git().commit("navegar entre registros info")
    await git().push('origin','sves');
 }
 

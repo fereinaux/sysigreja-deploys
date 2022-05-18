4fcdfd596c4cc13ba0979a2044e087813f45c019
Felipe Reinaux <freinaux@number8.com>
2022-05-17 23:15:33 -0300

    deletar equipanteevento

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('realidaderecon')
    await git().add('../.')
-   await git().commit("anexos da equipe")
+   await git().commit("deletar equipanteevento")
    await git().push('origin','realidaderecon');
 }
 

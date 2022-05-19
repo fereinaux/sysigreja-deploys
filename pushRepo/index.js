cb12472a36e92406573566822ac02cb40671ffc1
Felipe Reinaux <freinaux@number8.com>
2022-05-19 12:32:15 -0300

    resumo financeiro

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('sves')
    await git().add('../.')
-   await git().commit("navegar entre registros info")
+   await git().commit("resumo financeiro")
    await git().push('origin','sves');
 }
 

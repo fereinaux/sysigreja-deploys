0ad32b014133b1624974b2756e99fc974653bcf4
Felipe Reinaux <freinaux@number8.com>
2022-05-24 12:02:40 -0300

    mudança no yml

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("ajustes na ordenação, tirar coluna de foto, bloqueio na idade")
+   await git().commit("mudança no yml")
    await git().push('origin','ejc');
 }
 

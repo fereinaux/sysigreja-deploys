a73371343f7f9b09599b8fb34ba8b3576b04b25f
Felipe Reinaux <freinaux@number8.com>
2022-05-24 15:29:16 -0300

    webconfig

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("teste de deploy")
+   await git().commit("webconfig")
    await git().push('origin','ejc');
 }
 

abf1cefa88a56d2cf9cab6de701623316e30a484
Felipe Reinaux <freinaux@number8.com>
2022-06-06 11:53:43 -0300

    impressão etiquetas

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -1,10 +1,10 @@
 const git = require('simple-git');
 
 async function deploy() {   
-   await git().checkout('sves')
+   await git().checkout('realidade')
    await git().add('../.')
    await git().commit("impressão etiquetas")
-   await git().push('origin','sves');
+   await git().push('origin','realidade');
 }
 
 

ed823da2000ed27b1150fc98c0dd03f3fdb1917c
Felipe Reinaux <freinaux@number8.com>
2022-06-13 10:02:36 -0300

    mensagem para mãe/pai

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('realidade')
    await git().add('../.')
-   await git().commit("ajuste impressão equipes")
+   await git().commit("mensagem para mãe/pai")
    await git().push('origin','realidade');
 }
 

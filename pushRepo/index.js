25a55ad2062c635e0003c21691de9a5d5b8ad790
Felipe Reinaux <freinaux@number8.com>
2022-06-16 23:50:35 -0300

    ajuste quarto das equipes

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("imprimir tudo e refatoramento caronas")
+   await git().commit("ajuste quarto das equipes")
    await git().push('origin','ejc');
 }
 

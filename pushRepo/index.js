5e0da803a34051bc91efe4aca6c28570304bb685
Felipe Reinaux <felipereinaux@gmail.com>
2022-06-17 15:50:15 -0300

    quarto no checkin

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("ordem alfabética impressões")
+   await git().commit("quarto no checkin")
    await git().push('origin','ejc');
 }
 

b974ce5ae49a1e70a0c0979ff2ec59a0e81ef3fa
Felipe Reinaux <felipereinaux@gmail.com>
2022-06-17 15:57:58 -0300

    css checkin

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("quarto no checkin")
+   await git().commit("css checkin")
    await git().push('origin','ejc');
 }
 

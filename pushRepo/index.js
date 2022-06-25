6ab3e4345ec0fefcdf1d7a32c535bdac5aaf2bce
Felipe Reinaux <freinaux@number8.com>
2022-06-25 08:37:56 -0300

    Ajuste FirstOrDefault

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {   
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("Relatório do Quarto completo/resumido, Reponsável pelo quarto e colunas do excel personalizáveis")
+   await git().commit("Ajuste FirstOrDefault")
    await git().push('origin','ejc');
 }
 

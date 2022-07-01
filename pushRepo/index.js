647bef3103f2c7f7359aaa0a64fa987064d27e63
Felipe Reinaux <freinaux@number8.com>
2022-07-01 10:02:57 -0300

    ajuste impressão

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {
    await git().checkout('ejc')
    await git().add('../.')
-   await git().commit("ajuste anexo")
+   await git().commit("ajuste impressão")
    await git().push('origin', 'ejc');
 }
 

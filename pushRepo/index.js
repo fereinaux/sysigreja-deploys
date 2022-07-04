6be90c83b41af1810de0b0e0b6d8a42a357fe7da
Felipe Reinaux <freinaux@number8.com>
2022-07-04 17:41:45 -0300

    relatorio financeiro

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {
    await git().checkout('realidade')
    await git().add('../.')
-   await git().commit("remover campo centro de custo")
+   await git().commit("relatorio financeiro")
    await git().push('origin', 'realidade');
 }
 

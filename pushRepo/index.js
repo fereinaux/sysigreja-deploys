455a4b798ff2c8e6dd95c58a682b126dbea0d8e3
Felipe Reinaux <freinaux@number8.com>
2022-07-11 16:43:15 -0300

    correção inscrição Padrinhos

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {
    await git().checkout('sves')
    await git().add('../.')
-   await git().commit("Padrinhos")
+   await git().commit("correção inscrição Padrinhos")
    await git().push('origin', 'sves');
 }
 

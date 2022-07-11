c530f1e30df5b8dfe7bd15d783ca48325ab84756
Felipe Reinaux <freinaux@number8.com>
2022-07-11 18:35:21 -0300

    correção load Participante - Padrinhos

--- pushRepo/index.js
+++ pushRepo/index.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 async function deploy() {
    await git().checkout('sves')
    await git().add('../.')
-   await git().commit("correção inscrição Padrinhos")
+   await git().commit("correção load Participante - Padrinhos")
    await git().push('origin', 'sves');
 }
 

31c9c36abc578ebcc0876fec561beea6236cf3ee
Felipe Reinaux <felipereinaux@gmail.com>
2022-09-15 16:14:49 -0300

    alterações coord

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -163,7 +163,7 @@ namespace SysIgreja.Controllers
             var result = new
             {
                 Equipe = equipanteEvento.Equipe.Nome,
-                EquipeEnum = equipanteEvento.Equipe.Nome,
+                EquipeEnum = equipanteEvento.Equipe.Id,
                 QtdMembros = membrosEquipe.Count(),
                 Configuracao = new
                 {

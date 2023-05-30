a591ca129a006f7aa6fd4cc7290365b015c5dc90
Felipe Reinaux <felipereinaux@gmail.com>
2023-05-30 11:47:06 -0300

    deploy

--- Scripts/plugins/dataTables/datatable-default-config.js
+++ Scripts/plugins/dataTables/datatable-default-config.js
@@ -80,17 +80,17 @@ function getButtonsConfig(fileName) {
         },
         {
             extend: 'excel', title: fileName, exportOptions: {
-                columns: ':not(.noExport)'
+                columns: ':not(.noExport) :not(.noVis)'
             }
         },
         {
             extend: 'pdf', title: fileName, orientation: 'landscape', exportOptions: {
-                columns: ':not(.noExport)'
+                columns: ':not(.noExport) :not(.noVis)'
             }
         },
         {
             extend: 'print', title: fileName, text: "Imprimir", exportOptions: {
-                columns: ':not(.noExport)'
+                columns: ':not(.noExport) :not(.noVis)'
             }
         },
     ];

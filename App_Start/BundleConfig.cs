e8ecd675f8ed36112e6b9c6e5e857f20b8e27c95
Felipe Reinaux <felipereinaux@gmail.com>
2023-11-12 11:56:25 -0300

    deploy

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -90,7 +90,7 @@ namespace SysIgreja
                       "~/Scripts/plugins/dataTables/dataTables.responsive.min.js",
                       "~/Scripts/plugins/dataTables/dataTables.colReorder.min.js",
                       "~/Scripts/plugins/dataTables/vsfonts.js",
-                      "~/Scripts/plugins/dataTables/datatable-config-v11.js",
+                      "~/Scripts/plugins/dataTables/datatable-config-v12.js",
                       "~/Scripts/plugins/dataTables/dataTables.fixedColumns.min.js"));
 
             // dataPicker styles

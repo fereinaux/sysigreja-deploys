666c77f30a679ddf93b226731fffb8bf2b68be66
Felipe Reinaux <felipereinaux@gmail.com>
2024-02-02 15:15:00 -0300

    deploy

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ async function commit() {
   await git.simpleGit().commit('deploy')
   await git.simpleGit().push()
   console.log('commit done')
-  //iecb
 
-  Promise.all(
+  Promise.all([
+    //iecb
     axios.post('https://plesk6400.is.cc:8443/modules/git/public/web-hook.php?uuid=843290be-8694-27e8-3314-1639bfa03bd6')
     , axios.post('https://plesk6400.is.cc:8443/modules/git/public/web-hook.php?uuid=e5a99bd6-8e2e-2ffe-b733-c90f91848c8e')
     , axios.post('https://plesk6400.is.cc:8443/modules/git/public/web-hook.php?uuid=b2aa0709-303a-b350-586e-8910385f00a7')
@@ -20,7 +20,7 @@ async function commit() {
     , axios.post('https://plesk6400.is.cc:8443/modules/git/public/web-hook.php?uuid=ba236585-577f-2905-0bd0-bbedb42914a5')
     //sysigreja
     , axios.post('https://plesk6400.is.cc:8443/modules/git/public/web-hook.php?uuid=c4615387-45a9-6973-2f53-ef78f3dc2b4f')
-  ).then(() => {
+  ]).then(() => {
     console.log('webhook done')
   })
 }

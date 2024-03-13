8cdb9abc2b6b8079b2b87a853e2da5a30c3f4a0c
Felipe Reinaux <felipe.reinaux@thoughttrace.com>
2024-03-12 22:02:57 -0300

    deploy 12/03/2024 22:02:56

--- Recon.session.sql
+++
@@ -1,4 +0,0 @@
-select avg(contador) from (
-
-select count(*) as contador, month(DataEvento) as mes from Eventoes where YEAR(DataEvento) = 2023 group by month(DataEvento) order by 2 
-)
--- Scripts/app/components/DraggablePanels/DraggablePanelTitle.jsx
+++
@@ -1,8 +0,0 @@
-export function DraggablePanelTitle({ title, children }) {
-  return (
-    <>
-      <h4 style={{ paddingTop: "5px" }}>{title}</h4>
-      {children}
-    </>
-  );
-}
